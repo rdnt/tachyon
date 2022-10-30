28b5d4e5e420c3cca27aa1c8fff16f0ae0d26897
Tasos Papalyras <tasos@rdnt.dev>
2022-10-31 00:07:48 +0200

    Load config from json

--- cmd/tachyon/main.go
+++ cmd/tachyon/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"os/signal"
-	"time"
+	"log"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/go-redis/redis/v9"
 	"github.com/rdnt/tachyon/internal/application/command"
 	"github.com/rdnt/tachyon/internal/application/command/repository/project_repository"
@@ -20,32 +18,36 @@ import (
 )
 
 func main() {
+	cfg, err := LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	rdb := redis.NewClient(&redis.Options{
-		Addr: ":6379",
-		DB:   0,
+		Addr: fmt.Sprintf("%s:%d", cfg.Redis.Host, cfg.Redis.Port),
+		DB:   cfg.Redis.Database,
 	})
 
-	const redisStreamKey = "events"
-	redisClient := client.New(rdb, redisStreamKey)
+	redisClient := client.New(rdb, cfg.Redis.StreamKey)
 	eventStore := eventstore.New(redisClient)
 	eventBus := eventbus.New(redisClient)
-	//eventBus := event_bus.New(fanout.New[event.Event]())
 
-	//eventStore := event_store.New()
 	sessionRepo, err := session_repository.New(eventStore)
 	if err != nil {
-		panic(err)
-	}
-	userRepo, err := user_repository.New(eventStore)
-	if err != nil {
-		panic(err)
-	}
-	projectRepo, err := project_repository.New(eventStore)
-	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	commandSvc := command.New(
+	userRepo, err := user_repository.New(eventStore)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	projectRepo, err := project_repository.New(eventStore)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	commands := command.New(
 		eventStore,
 		eventBus,
 		sessionRepo,
@@ -55,118 +57,44 @@ func main() {
 
 	sessionView, err := session_repository.New(eventBus)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	userView, err := user_repository.New(eventBus)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	projectView, err := project_repository.New(eventBus)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	querySvc := query.New(
+	queries := query.New(
 		eventBus,
 		sessionView,
 		userView,
 		projectView,
 	)
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
+	uid := uuid.Nil
+	err = commands.CreateUser(uid, "user-1")
+	fmt.Println(err)
 
-	go func() {
-		_ = commandSvc
-		_ = querySvc
+	pid := uuid.Nil
+	err = commands.CreateProject(pid, "project-1", uid)
+	fmt.Println(err)
 
-		for {
-			fmt.Print("> ")
-			input := bufio.NewScanner(os.Stdin)
-			input.Scan()
-			cmd := input.Text()
+	m := &model{
+		commands:  commands,
+		queries:   queries,
+		projectId: pid,
+	}
 
-			fmt.Println()
+	p := tea.NewProgram(m, tea.WithAltScreen(), tea.WithMouseAllMotion())
 
-			switch cmd {
-			case "store":
-				fmt.Println(eventStore)
-			case "bus":
-				fmt.Println(eventBus)
-			case "repo":
-				fmt.Println("sessions:", sessionRepo)
-				fmt.Println("users:", userRepo)
-				fmt.Println("projects:", projectRepo)
-			case "view":
-				fmt.Println("sessions:", sessionView)
-				fmt.Println("users:", userView)
-				fmt.Println("projects:", projectView)
-			case "create user":
-				uid := uuid.New()
-				err := commandSvc.CreateUser(uid, "user-1")
-				if err != nil {
-					panic(err)
-				}
-
-				ru, err := userRepo.User(uid)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println("USER FOUND", ru)
-
-				//err = userRepo.Hydrate()
-				//if err != nil {
-				//	panic(err)
-				//}
-
-				//for i := 0; i < 1000; i++ {
-				//	u, err := querySvc.User("user-1")
-				//	if err != nil {
-				//		fmt.Println(err)
-				//		continue
-				//	}
-				//
-				//	fmt.Println(u)
-				//	break
-				//}
-
-			case "create project":
-				pid := uuid.New()
-
-				fmt.Println("name?")
-				input.Scan()
-				name := input.Text()
-
-				fmt.Println("ownerId?")
-				input.Scan()
-				uid, err := uuid.Parse(input.Text())
-				if err != nil {
-					panic(err)
-				}
-
-				err = commandSvc.CreateProject(pid, name, uid)
-				if err != nil {
-					panic(err)
-				}
-
-			//case "create session":
-			//	err := commandSvc.CreateSession("someUserId", "someProjectId", "my-session")
-			//	if err != nil {
-			//		panic(err)
-			//	}
-			default:
-				fmt.Println("invalid command")
-			case "quit":
-				close(c)
-			}
-
-			time.Sleep(1 * time.Millisecond)
-
-			fmt.Println()
-		}
-	}()
-
-	<-c
+	err = p.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
